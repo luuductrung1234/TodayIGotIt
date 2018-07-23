1d9e40d5e10d5ec79c770e65904b48914a100074
ldt <luuductrung1234@gmail.com>
2018-07-23 11:46:22 +0700

    > ADD : new TestController

---
+++ springmvc/src/main/java/ldt/springframework/springmvc/controller/TestController.java
@@ -0,0 +1,16 @@
+package ldt.springframework.springmvc.controller;
+
+/*
+ * author: Luu Duc Trung
+ * https://github.com/luuductrung1234
+ * ---
+ * 7/23/18
+ */
+public class TestController {
+
+
+    // blah blah
+
+
+
+}
