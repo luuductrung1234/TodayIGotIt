6002e80c3baafc8e31c1d9a9d5290e875e49aa33
Lưu Đức Trung <luuductrung1234@gmail.com>
2018-08-04 13:43:29 +0700

    > UPDATE : frontend modify loginAction(), modify cors() problem in spring security config

---
+++ tigiproject/tigi-business/src/main/java/ldt/springframework/tigibusiness/TigiBusinessApplication.java
@@ -0,0 +1,13 @@
+package ldt.springframework.tigibusiness;
+
+/*
+ * author: Luu Duc Trung
+ * https://github.com/luuductrung1234
+ * ---
+ * 8/3/18
+ */
+public class TigiBusinessApplication {
+    public static void main(String[] args){
+
+    }
+}
