ef969de7d862879e6835881d07e30f407fbddcb1
Lưu Đức Trung <luuductrung1234@gmail.com>
2018-08-09 10:40:26 +0700

    > UPDATE

---
+++ tigiproject/tigi-business/src/main/java/ldt/springframework/tigibusiness/services/machineLearning/MlpManchineLearningService.java
@@ -0,0 +1,10 @@
+package ldt.springframework.tigibusiness.services.machineLearning;
+
+/*
+ * author: Luu Duc Trung
+ * https://github.com/luuductrung1234
+ * ---
+ * 8/9/18
+ */
+public interface MlpManchineLearningService {
+}
