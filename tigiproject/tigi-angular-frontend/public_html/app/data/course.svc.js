a646ac9108ae5573e7408797954b06947723622d
unknown <kaitodeluxelupin@gmail.com>
2018-08-09 10:38:42 +0700

    > UPDATE : update frontend - course learning

--- tigiproject/tigi-angular-frontend/public_html/app/data/course.svc.js
+++ tigiproject/tigi-angular-frontend/public_html/app/data/course.svc.js
@@ -7,7 +7,8 @@
                 getIntroVideo: getIntroVideo,
                 getImage: getImage,
                 getAllReview: getAllReview,
-                // getCurInstructor: getCurInstructor,
+                getAllResources: getAllResources
+                    // getCurInstructor: getCurInstructor,
             }
 
             function getIntroVideo(id, username, password) {
@@ -113,36 +114,58 @@
             }
 
             function getCurInstructor(id) {
-                var alluser = [];
-                var ins = {};
+                var url = serverUrl + "course/" + id + "/review";
 
                 var deferred = $q.defer();
 
-                UserSvc.findAllUser()
-                    .then(function(response) {
-                        alluser = response;
-                        alluser.forEach(i => {
-                            i.userCourseOwner.forEach(ii => {
-                                if (ii.id == id) {
-                                    if (ii.owerType == 'CREATE') {
-                                        ins = ii;
-                                        deferred.resolve(ins);
-                                    }
-                                }
-                            });
-                        });
-                        // alluser.forEach(function(i) {
-                        //     i.userCourseOwner.forEach(function(ii) {
-                        //         if (ii.id == id) {
-                        //             if (ii.owerType == 'CREATE') {
-                        //                 ins = ii;
-                        //                 deferred.resolve(ins);
-                        //             }
-                        //         }
-                        //     })
-                        // })
-                    }, function(err) {
-                        console.log("Error: " + err);
+                return deferred.promise;
+            }
+
+            function getAllResources(id, username, password) {
+                var auth = btoa(`${username}:${password}`);
+
+                var url = serverUrl + "course/" + id + "/resources";
+
+                var deferred = $q.defer();
+
+                $http({
+                        method: 'GET',
+                        url: url,
+                        headers: {
+                            'Authorization': 'Basic ' + auth
+                        }
+                    })
+                    .success(function(response) {
+                        console.log(response);
+                        deferred.resolve(response);
+                    })
+                    .error(function(err) {
+                        deferred.reject(err);
+                    });
+
+                return deferred.promise;
+            }
+
+            function getResourcesVideo(id, username, password) {
+                var auth = btoa(`${username}:${password}`);
+
+                var url = serverUrl + "course/resource/" + id + "/media/video";
+
+                var deferred = $q.defer();
+
+                $http({
+                        method: 'GET',
+                        url: url,
+                        headers: {
+                            'Authorization': 'Basic ' + auth
+                        }
+                    })
+                    .success(function(response) {
+                        console.log(response);
+                        deferred.resolve(response);
+                    })
+                    .error(function(err) {
+                        deferred.reject(err);
                     });
 
                 return deferred.promise;
